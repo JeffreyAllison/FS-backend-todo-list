1371a36d18ab5429e5c625e6091eb8a5186c7fc3
JeffAllison <itsjallison@gmail.com>
2022-06-28 14:59:53 -0700

    checked out to dev branch, imported extentions, created necessary files, setup SQL tables

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,13 +3,11 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS todo_users;
+
+CREATE TABLE todo_users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  task_name TEXT,
+  description VARCHAR NOT NULL,
+  bought BOOLEAN NOT NULL DEFAULT(true),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
